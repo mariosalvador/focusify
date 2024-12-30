12bc70c392bf739b858ee81f79f17c241d52ad62
Mário Salvador <128391174+mariosalvador@users.noreply.github.com>
2024-12-30 15:26:51 +0000

    feat: :sparkles: add Privacy Policy and Terms pages with placeholder content

---
+++ src/app/(apk)/privacy-policy/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function PrivacyPolitics() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center overflow-y-auto">
+      <h1 className="text-3xl font-bold">Privacy and Policies will be defined</h1>
+    </div>
+  );
+}
---
+++ src/app/(apk)/terms/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function TermsPage() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center overflow-y-auto">
+      <h1 className="text-3xl font-bold">Terms and Conditions will be defined</h1>
+    </div>
+  );
+}
